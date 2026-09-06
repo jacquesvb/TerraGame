10496bb55b9a97272e0f4a75b004b705c147c564
Jacques Van Blokland <jacquesvb@live.com>
2026-09-05 19:10:02 -0700

    Camera

--- src/gameLayer/gameMain.cpp
+++ src/gameLayer/gameMain.cpp
@@ -7,6 +7,7 @@
 struct GameData
 {
 	GameMap gameMap;
+	Camera2D camera;
 
 }gameData;
 
@@ -26,6 +27,10 @@ bool initGame()
 	gameData.gameMap.getBlocUnsafe(3, 3).type = Block::dirt;
 	gameData.gameMap.getBlocUnsafe(4, 4).type = Block::dirt;
 
+	gameData.camera.target = {0, 0};  // world-space center of view, we will use this as the camera position
+	gameData.camera.rotation = 0.0f;
+	gameData.camera.zoom = 100.0f;
+	
 	return true;
 }
 
@@ -34,7 +39,23 @@ bool updateGame()
 	float deltaTime = GetFrameTime();
 	if (deltaTime > 1.f / 5) { deltaTime = 1 / 5.f; }
 
+	gameData.camera.offset = {GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f};
+
 	ClearBackground({75, 75, 150, 255});
+
+
+#pragma region camera movement
+
+	if (IsKeyDown(KEY_LEFT)) gameData.camera.target.x -= 7.f * deltaTime;
+	if (IsKeyDown(KEY_RIGHT)) gameData.camera.target.x += 7.f * deltaTime;
+	if (IsKeyDown(KEY_UP)) gameData.camera.target.y -= 7.f * deltaTime;
+	if (IsKeyDown(KEY_DOWN)) gameData.camera.target.y += 7.f * deltaTime;
+
+#pragma endregion
+
+#pragma region draw world
+
+	BeginMode2D(gameData.camera);
 
 	for (int y = 0; y < gameData.gameMap.h; y++)
 		for (int x = 0; x < gameData.gameMap.w; x++)
@@ -44,7 +65,8 @@ bool updateGame()
 
 			if (b.type != Block::air)
 			{
-				float size = 32;
+				//todo remove this useless size here
+				float size = 1;
 				float posX = x * size;
 				float posY = y * size;
 
@@ -56,8 +78,18 @@ bool updateGame()
 					0.0f, // rotation
 					WHITE // tint
 				);
+
 			}
+
+
 		}
+
+	EndMode2D();
+
+
+#pragma endregion
+	
+
 	return true;
 }
 
--- src/platform/main.cpp
+++ src/platform/main.cpp
@@ -1,60 +1,36 @@
 #include <iostream>
 #include <raylib.h>
+
 #include <imgui.h>
 #include <rlImGui.h>
 
-#include "gameMain.h"
+#include <gameMain.h>
 
-float DetectLinuxScale()
-{
-	const char* gdkScale = getenv("GDK_SCALE");
-	if (gdkScale) return (float)atof(gdkScale);
-
-	const char* qtScale = getenv("QT_SCALE_FACTOR");
-	if (qtScale) return (float)atof(qtScale);
-
-	return 1.0f; // fall back to no scaling
-}
 
 int main(void)
 {
-	#if PRODUCTION_BUILD == 1
-		SetTraceLogLevel(LOG_NONE); // Disable logging in production builds
-	#endif
 
-	// FLAG_WINDOW_HIGHDPI tells the OS window backend this app is DPI-aware
-	SetConfigFlags(FLAG_WINDOW_HIGHDPI |FLAG_WINDOW_RESIZABLE | FLAG_MSAA_4X_HINT);
-	InitWindow(1920, 1080, "window name");
-	SetExitKey(KEY_NULL); // Disable the default exit key (ESC) to prevent accidental closure
-	SetTargetFPS(240); // Set a high target FPS for smooth rendering
+#if PRODUCTION_BUILD == 1
+	SetTraceLogLevel(LOG_NONE); // no log output to the console by raylib
+#endif
 
-	float dpiScale = DetectLinuxScale();
-	float widgetScale = 3.0f; // Scale ImGui widgets to be larger on high-DPI displays
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	InitWindow(800, 450, "window name");
+	SetExitKey(KEY_NULL); // Disable Esc from closing window
+	SetTargetFPS(240);
 
-	// Load the font at a size multiplied by the DPI scale
-	const int baseFontSize = 40;
-	const int scaledFontSize = (int)(baseFontSize * dpiScale);
-
-	Font roboto = LoadFontEx("resources/Roboto-Regular.ttf", scaledFontSize, nullptr, 0);
-	SetTextureFilter(roboto.texture, TEXTURE_FILTER_BILINEAR);
-
-#pragma region imgui	
+#pragma region imgui
 	rlImGuiSetup(true);
-	
-	ImGuiIO& io = ImGui::GetIO();
-	// io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable keyboard controls
-	// io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable gamepad controls
-	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;      // Enable docking
-	io.FontGlobalScale = widgetScale; // Scale ImGui's own font rendering to match the display's DPI
 
-	// ImGui::StyleColorsDark();
-	// ImGui::StyleColorsLight();
-	ImGui::StyleColorsClassic();
+
+	ImGuiIO &io = ImGui::GetIO(); (void)io;
+	////io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
+	////io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;       // Enable gamepad Controls
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+	io.FontGlobalScale = 2;
+
 #pragma endregion
-	
-	// Scale all ImGui widget sizes, padding, borders, etc. to match the display's DPI
-	ImGuiStyle& style = ImGui::GetStyle();
-	style.ScaleAllSizes(widgetScale);
+
 
 	if (!initGame())
 	{
@@ -69,7 +45,7 @@ int main(void)
 	#pragma region imgui
 		rlImGuiBegin();
 
-		// docking stuff
+		//docking stuff
 		ImGui::PushStyleColor(ImGuiCol_WindowBg, {});
 		ImGui::PushStyleColor(ImGuiCol_DockingEmptyBg, {});
 		ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
@@ -83,18 +59,20 @@ int main(void)
 
 	#pragma region imgui
 		rlImGuiEnd();
+
 	#pragma endregion
-		
+
 		EndDrawing();
 	}
 
 	CloseWindow();
-	UnloadFont(roboto);
+
 	closeGame();
 
 #pragma region imgui
 	rlImGuiShutdown();
 #pragma endregion
+
 
 	return 0;
 }
